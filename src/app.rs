99243fc2d55f9047b7fce4e53fde21dd955e85fc
Charles Taylor <charlestaylor95@gmail.com>
2023-03-05 13:36:41 -0500

    Works

--- src/app.rs
+++ src/app.rs
@@ -78,7 +78,8 @@ fn Game(cx: Scope) -> impl IntoView {
 
     create_effect(cx, move |_| {
         let result = set_interval(
-            move || seconds.update(|s| seconds.set(*s + 1)),
+            // move || seconds.update(|s| seconds.set(*s + 1)),
+            move || seconds.set(10),
             Duration::new(1, 0),
         );
 
