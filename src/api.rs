20b59c1151d2cd3fd1e249859f4a9815e03a3777
Charles Taylor <charlestaylor95@gmail.com>
2023-03-06 18:15:28 -0500

    feat: State management; can join game

--- src/api.rs
+++ src/api.rs
@@ -1,36 +1,47 @@
 use crate::types::*;
 use leptos::*;
-use std::sync::{Mutex, Arc};
-//use std::sync::mpsc::*;
-
+use std::sync::*;
 
 #[cfg(feature = "ssr")]
 lazy_static::lazy_static! {
     pub static ref STATE: Arc<Mutex<GameState>> = Arc::new(Mutex::new(Default::default()));
 }
 
-
 #[cfg(feature = "ssr")]
 pub fn register_server_functions() {
     _ = FetchPlayers::register();
     _ = FetchGameStep::register();
+    _ = JoinGame::register();
 }
 
 // Apis
 /// get the players in the game
 #[server(FetchPlayers, "/api")]
-pub async fn fetch_players(cx: Scope, room_code: String) -> Result<Vec<Player>, ServerFnError> {
+pub async fn fetch_players(cx: Scope, _room_code: String) -> Result<Vec<Player>, ServerFnError> {
     let http_request = use_context::<actix_web::HttpRequest>(cx);
     dbg!(http_request);
-    let state = Arc::clone(&STATE);
-    let state = state.lock().expect("locking thread crashed");
+
+    let state = STATE.lock().expect("locking thread crashed");
 
     Result::Ok(state.players.clone())
 }
 
 /// get the current game state
 #[server(FetchGameStep, "/api")]
-pub async fn fetch_game_step(room_code: String) -> Result<GameStep, ServerFnError> {
-    // pretend we're fetching game state
-    Result::Ok(GameStep::Setup)
+pub async fn fetch_game_step(_room_code: String) -> Result<GameStep, ServerFnError> {
+    let state = STATE.lock().expect("locking thread crashed");
+
+    Result::Ok(state.step.clone())
+}
+
+
+/// register for the game the current game state
+#[server(JoinGame, "/api")]
+pub async fn join_game(name: String) -> Result<(), ServerFnError> {
+    let mut state = STATE.lock().expect("locking thread crashed");
+
+    let length = state.players.len();
+    state.players.push(Player{id: length.try_into().unwrap(), name: name});
+
+    Result::Ok(())
 }
--- src/components/home_page.rs
+++ src/components/home_page.rs
@@ -1,6 +1,7 @@
 use crate::components::text_input::*;
 use leptos::*;
 use leptos_router::*;
+use crate::api::*;
 
 /// The home page allows you to:
 /// - Set your nickname
@@ -9,7 +10,8 @@ use leptos_router::*;
 pub fn HomePage(cx: Scope) -> impl IntoView {
     let name = create_rw_signal::<String>(cx, "boaty_mcboatface".to_owned());
     let room_code = create_rw_signal::<String>(cx, "abc".to_owned());
-
+    let join = create_action(cx, move |name: &String| join_game(name.clone()));
+    let navigate = use_navigate(cx);
     view! {
         cx,
         <h1>"Welcome to Acronymia!"</h1>
@@ -18,11 +20,14 @@ pub fn HomePage(cx: Scope) -> impl IntoView {
 
         "Enter your room code: "
         <TextInput signal=room_code />
-        <A
-            href=move|| format!("/game/{}?name={}", room_code.get(), name.get())
+        <button
+            on:click=move |_| {
+                join.dispatch(name.get());
+                navigate(&format!("/game/{}?name={}", room_code.get(), name.get()), Default::default());
+            }
         >
             "Join!"
-        </A>
+        </button>
         <p>{ name }</p>
     }
 }
