fb3bb3f10279f1bdf43406df06cf88a1012e2725
Charles Taylor <charlestaylor95@gmail.com>
2023-03-10 22:01:39 -0500

    Reset applies immediately

--- src/components/reset_button.rs
+++ src/components/reset_button.rs
@@ -1,8 +1,12 @@
+use crate::*;
 use leptos::*;
 
 #[component]
 pub fn ResetButton(cx: Scope) -> impl IntoView {
-    let reset = create_action(cx, move |_: &()| crate::api::reset_state());
+    let reset = create_action(cx, move |_: &()| {
+        sse::game_state(cx).update(|g| *g = Default::default());
+        api::reset_state()
+    });
 
     view! {
         cx,
