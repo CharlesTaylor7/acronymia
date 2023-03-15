75eaf7f952075a368df54bf14ae33e15c4af4f39
Spencer Dent <mr.spencerdent@gmail.com>
2023-03-15 16:25:40 -0400

    feat: create initial judging view

--- src/components/game/judging.rs
+++ src/components/game/judging.rs
@@ -1,9 +1,47 @@
 use leptos::*;
 
 #[component]
-pub fn GameJudging(_cx: Scope) -> impl IntoView {
+pub fn GameJudging(cx: Scope) -> impl IntoView {
+    let (submissions, _set_submissions) = create_signal::<Vec<(usize, String)>>(
+        cx,
+        vec![
+            "A Stupid Submission",
+            "Another Sexy Shape",
+            "haha butts",
+            "Adamant, Stubborn Soil-till",
+        ]
+        .iter()
+        .map(|&s| s.to_owned())
+        .enumerate()
+        .collect(),
+    );
+
     view! {
         cx,
-        "Judging!"
+        <>
+            <header>"Judging!"</header>
+
+            <main>
+                <fieldset>
+                    <legend>"Please select the best submission:"</legend>
+
+                    <For
+                        each=submissions
+                        key=|(i, _)| i.clone()
+                        view=move |cx, (i, submission): (usize, String)| {
+                            view! {
+                                cx,
+                                <div>
+                                    <input type="radio" name="best" id={ i.clone() } value={ submission.clone().to_owned() } />
+                                    <label for={ i.clone() }>{ submission.to_owned() }</label>
+                                </div>
+                            }
+                        }
+                    />
+                </fieldset>
+
+                <button>"Select"</button>
+            </main>
+        </>
     }
 }
