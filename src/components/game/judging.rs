161cd1e41acd93bd8cdf02a46448d4056d0faa6e
Charles Taylor <charlestaylor95@gmail.com>
2023-03-16 16:14:05 -0400

    Restyle Judging view

--- src/components/game/judging.rs
+++ src/components/game/judging.rs
@@ -1,41 +1,50 @@
-use leptos::*;
-
 use crate::components::game::utils::state::*;
+use crate::types::*;
+use leptos::*;
 
 #[component]
 pub fn GameJudging(cx: Scope) -> impl IntoView {
-    let submissions = Signal::derive(cx, move || {
-        game_state(cx)()
-            .submissions
-            .into_iter()
-            .map(|t| t.1)
-            .enumerate()
-            .collect::<Vec<(usize, Vec<String>)>>()
-    });
-
-    // I still need to grab the select size from the number of submissions
+    let selected = create_rw_signal(cx, None);
+    let submissions = move || game_state(cx).with(|g| g.submissions.clone());
+    let option_class = move |id: &PlayerId| {
+        let id = id.clone();
+        move || {
+            format!(
+                "cursor-pointer p-2 border rounded border-slate-300 {}",
+                if selected() == Some(id.clone()) {
+                    "bg-blue-200"
+                } else {
+                    "bg-slate-200 hover:bg-blue-300"
+                }
+            )
+        }
+    };
 
     view! {
         cx,
-        <>
-            <header>"Judging!"</header>
+        <div class="flex flex-col items-start gap-4">
+            <header>"What is "<span class="inline font-bold">"F.O.O.?"</span></header>
+            <For
+                each=submissions
+                key=|(id, _)| id.clone()
+                view=move |cx, (id, words)| {
+                    view! {
+                        cx,
+                        <button
+                            class=option_class(&id)
+                            on:click=move|_| selected.set(Some(id.clone()))
+                        >
+                            {words.join(" ")}
+                        </button>
+                    }
+                }
+            />
 
-            <main>
-                <select size="2" required="required">
-                    <For
-                        each=submissions
-                        key=|(i, _)| i.clone()
-                        view=move |cx, (_i, words): (usize, Vec<String>)| {
-                            view! {
-                                cx,
-                                <option value={ words.clone().join(" ") }>{ words.join(" ") }</option>
-                            }
-                        }
-                    />
-                </select>
-
-                <button>"Select"</button>
-            </main>
-        </>
+            <button
+                class="border rounded p-2 bg-green-300 border-slate-200"
+            >
+            "Submit"
+            </button>
+        </div>
     }
 }
