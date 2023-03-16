c702b410363c8e697bb419c359e2e6a0e7d5b69f
Spencer Dent <mr.spencerdent@gmail.com>
2023-03-16 12:38:40 -0400

    feat: display game_state submissions to judge

--- src/components/game/judging.rs
+++ src/components/game/judging.rs
@@ -1,20 +1,17 @@
 use leptos::*;
 
+use crate::components::game::utils::state::*;
+
 #[component]
 pub fn GameJudging(cx: Scope) -> impl IntoView {
-    let (submissions, _set_submissions) = create_signal::<Vec<(usize, String)>>(
-        cx,
-        vec![
-            "A Stupid Submission",
-            "Another Sexy Shape",
-            "haha butts",
-            "Adamant, Stubborn Soil-till",
-        ]
-        .iter()
-        .map(|&s| s.to_owned())
-        .enumerate()
-        .collect(),
-    );
+    let submissions = Signal::derive(cx, move || {
+        game_state(cx)()
+            .submissions
+            .into_iter()
+            .map(|t| t.1)
+            .enumerate()
+            .collect::<Vec<(usize, Vec<String>)>>()
+    });
 
     view! {
         cx,
@@ -28,12 +25,12 @@ pub fn GameJudging(cx: Scope) -> impl IntoView {
                     <For
                         each=submissions
                         key=|(i, _)| i.clone()
-                        view=move |cx, (i, submission): (usize, String)| {
+                        view=move |cx, (i, words): (usize, Vec<String>)| {
                             view! {
                                 cx,
                                 <div>
-                                    <input type="radio" name="best" id={ format!("submission_{i}") } value={ submission.clone().to_owned() } checked={ i == 0 } required="required" />
-                                    <label for={ format!("submission_{i}") }>{ submission.to_owned() }</label>
+                                    <input type="radio" name="best" id={ format!("submission_{i}") } value={ words.clone().join(" ") } checked={ i == 0 } required="required" />
+                                    <label for={ format!("submission_{i}") }>{ words.join(" ") }</label>
                                 </div>
                             }
                         }
