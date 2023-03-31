112238097a29632ac30516363ea60e21909d3fb2
Charles Taylor <charlestaylor95@gmail.com>
2023-03-31 10:38:00 -0400

    Efficiency

--- macros-impl/src/sliceable.rs
+++ macros-impl/src/sliceable.rs
@@ -1,34 +1,33 @@
 use ::proc_macro::TokenStream;
 use ::quote::{format_ident, quote};
-use ::syn::{punctuated::*, token::Comma, Data::*, Fields::*, Type, *};
+use ::syn::{punctuated::*, token::Comma, Fields::*, Type, *};
 
 pub fn derive(ast: DeriveInput) -> TokenStream {
-    let vis = &ast.vis;
-    let struct_name = &ast.ident;
+    let vis = ast.vis.clone();
+    let struct_name = ast.ident.clone();
     let sliced_struct_name = format_ident!("{}_Sliced", struct_name);
-    let fields = fields(&ast);
-    let sliced_fields_def = fields
-        .iter()
+    let sliced_fields_def = fields(ast)
+        .into_iter()
         .map(|f| {
             let ty = &f.ty;
             Field {
                 ty: Type::Verbatim(quote! {
                     ::macros::slice::JoinedSignal<#ty>
                 }),
-                ..f.clone()
+                ..f
             }
         })
-        .collect::<Punctuated<Field, Comma>>();
+        .collect::<Vec<_>>();
 
-    let field_names = fields
-        .into_iter()
+    let field_names = sliced_fields_def
+        .iter()
         .map(|f| f.ident.as_ref().unwrap())
-        .collect::<Vec<&Ident>>();
+        .collect::<Vec<_>>();
 
     quote! {
         #[allow(non_camel_case_types)]
         #vis struct #sliced_struct_name {
-            #sliced_fields_def
+            #(#sliced_fields_def,)*
         }
         impl Sliceable for #struct_name {
             type Sliced = #sliced_struct_name;
@@ -49,15 +48,15 @@ pub fn derive(ast: DeriveInput) -> TokenStream {
     .into()
 }
 
-fn fields(ast: &DeriveInput) -> &Punctuated<Field, Comma> {
-    let data = if let Data::Struct(data) = &ast.data {
+fn fields(ast: DeriveInput) -> Punctuated<Field, Comma> {
+    let data = if let Data::Struct(data) = ast.data {
         data
     } else {
         panic!("Sliceable can only be derived for structs with named fields .")
     };
 
-    if let Named(FieldsNamed { named, .. }) = &data.fields {
-        &named
+    if let Named(FieldsNamed { named, .. }) = data.fields {
+        named
     } else {
         panic!("Sliceable can only be derived for structs with named fields .")
     }
