2d307699a58e8e84b65f9d04047c08a948282300
Charles Taylor <charlestaylor95@gmail.com>
2023-08-25 12:27:46 -0400

    Use typed event listener api

--- src/client/timer.rs
+++ src/client/timer.rs
@@ -4,5 +4,5 @@ use leptos::*;
 /// When the window regains focus, request the current server time.
 pub fn auto_sync_with_server(cx: Scope) {
     let request = create_action(cx, move |_| send(cx, GetRemainingTime));
-    window_event_listener("focus", move |_| request.dispatch(()));
+    window_event_listener(ev::focus, move |_| request.dispatch(()));
 }
