253ff85de1180e1d5c1807dfba765b5b72aee904
Charles Taylor <charlestaylor95@gmail.com>
2023-08-25 15:09:20 -0400

    Drop playwright from testsuite

--- end2end/tests/example.spec.ts
+++ end2end/tests/example.spec.ts
@@ -7,8 +7,5 @@ test("homepage has title and links to intro page", async ({ page }) => {
   await page.getByTestId("input-nickname").fill("Bob");
   await page.getByRole('button', { name: 'Join' }).click(); 
 
-  // TODO: debug this
-  // My working theory is that the websocket is failing to connect,
-  // which leaves the app in its default state.
-  // await expect(page.getByTestId('player-Bob')).toBeVisible()
+  await expect(page.getByTestId('player-Bob')).toBeVisible()
 });
