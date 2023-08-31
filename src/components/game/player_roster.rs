ca5f1293de13ad95d27d39b9e7feab205b18a1d0
Charles Taylor <charlestaylor95@gmail.com>
2023-08-31 12:07:13 -0400

    Hide impersonate button in production

--- src/components/game/player_roster.rs
+++ src/components/game/player_roster.rs
@@ -1,5 +1,6 @@
 use super::context::*;
 use crate::components::game::*;
+use crate::constants::DEV_MODE;
 use crate::types::ClientMessage::*;
 use crate::types::*;
 use ::leptos::*;
@@ -7,7 +8,6 @@ use ::leptos::*;
 #[component]
 pub fn PlayerRoster() -> impl IntoView {
     let players = use_typed_context::<Memo_Players>();
-    let impersonate = SignalSetter::from(use_typed_context::<Signal_PlayerId>());
 
     view! {
         <ul class="gap-3 list-inside list-disc flex flex-col items-start" >
@@ -17,7 +17,6 @@ pub fn PlayerRoster() -> impl IntoView {
                 view=move |p| view! {
                     <PlayerView
                         player=p
-                        impersonate=impersonate
                     />
                 }
             />
@@ -26,29 +25,35 @@ pub fn PlayerRoster() -> impl IntoView {
 }
 
 #[component]
-fn PlayerView(player: Player, impersonate: SignalSetter<Option<PlayerId>>) -> impl IntoView {
+fn PlayerView(player: Player) -> impl IntoView {
     let player_id = use_typed_context::<Signal_PlayerId>();
     let action = create_ws_action();
 
     // TODO: why do I have to clone this variable so many times?
     // If I try to only once in each callback, I get weird ownership errors.
-    let id1 = player.id.clone();
-    let id2 = player.id.clone();
-    let id3 = player.id.clone();
+    let stored_id = store_value(player.id);
+    let impersonate = move || player_id.set(Some(stored_id.get_value()));
+    let kick = move || action.dispatch(KickPlayer(stored_id.get_value()));
+    let disabled_kick =
+        move || stored_id.with_value(|id1| player_id.with(|id2| Some(id1) == id2.as_ref()));
     view! {
-
         <li>
             {player.name}
-            <button
-                class="bg-cyan-500 text-blue-50 rounded mx-2 px-2 disabled:bg-slate-600"
-                on:click=move|_| impersonate.set(Some(id1.clone()))
+            <Show
+                fallback=|| ()
+                when=|| DEV_MODE
             >
-                "Impersonate"
-            </button>
+                <button
+                    class="bg-cyan-500 text-blue-50 rounded mx-2 px-2 disabled:bg-slate-600"
+                    on:click=move|_| impersonate()
+                >
+                    "Impersonate"
+                </button>
+            </Show>
             <button
                 class="bg-rose-400 text-blue-50 rounded mx-2 px-2 disabled:bg-slate-600"
-                disabled=move|| Some(id3.clone()) == player_id.get() // can't kick self
-                on:click=move|_| action.dispatch(KickPlayer(id2.clone()))
+                disabled=disabled_kick
+                on:click=move|_| kick()
             >
                 "Kick"
             </button>
