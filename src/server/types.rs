8c5903f087b6c47601ac16a31172f5887b13d791
Charles Taylor <charlestaylor95@gmail.com>
2023-09-01 09:08:21 -0400

    remove unused

--- src/server/types.rs
+++ src/server/types.rs
@@ -304,7 +304,6 @@ mod tests {
     use crate::server::types::*;
     #[test]
     fn scores() {
-        let state = GameState::default();
         let state = GameState {
             rounds: vec![
                 Round {
@@ -363,7 +362,6 @@ mod tests {
             ],
             ..demo_init(vec!["a", "b", "c"])
         };
-        let scores = state.scores();
         assert_eq!(
             state.scores(),
             vec![
