ae3219208896c8eee0ef46257459e5bf5ad28432
Charles Taylor <charlestaylor95@gmail.com>
2024-01-03 10:29:12 -0500

    Fix comment

--- macros/src/tag.rs
+++ macros/src/tag.rs
@@ -1,7 +1,7 @@
 pub use macros_impl::Tag;
 
-/// Derivable for structs with named fields that implement
-/// PartialEq & Clone.
+/// Derivable for any enum
+/// Generates a enum with unitary variants for each case of the original enum
 pub trait Tag: Sized {
     type Tag: std::fmt::Debug + Clone + PartialEq;
     fn to_tag(&self) -> Self::Tag;
